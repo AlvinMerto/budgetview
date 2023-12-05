832881b7c70eb6b5773fc2b59bcf119b6eebadfd
Alvin Merto <ajbmerto@gmail.com>
2023-12-05 08:23:12 +0800

    updated caps

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\BudgetviewController;
 use App\Http\Controllers\InputwindowController;
 use App\Http\Controllers\LoginControlController;
