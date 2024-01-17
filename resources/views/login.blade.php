7dfa291f778f41d8b48832678250fe263b39d4c5
Alvin Merto <ajbmerto@gmail.com>
2024-01-17 14:20:07 +0800

    with edits to logo

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -11,7 +11,7 @@
 <!-- Automatic element centering -->
 <div class="lockscreen-wrapper">
   <div class="lockscreen-logo">
-  	Policy, Planning, Knowledge and Project Development Office <br/> <b>(PPPKDO)</b>
+  	<img class="animation__wobble" src="dist/img/PPPDO_minda.png" alt="PPPDO Budget System" height="45" width="210" style="margin-left: 10px;">
     <!-- <a href="../../index2.html"><b>Admin</b>LTE</a> -->
   </div>
   <!-- User name -->
